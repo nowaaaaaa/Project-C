9dce06828f7eedf513f7e20d9824dfa42f1d2028
nowa <57222741+nowaaaaaa@users.noreply.github.com>
2022-09-07 13:57:58 +0200

    hallo wereld
    de wereld is van mij

---
+++ Service/Service/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Service {
+    public class Program {
+        public static void Main() {
+            Console.BackgroundColor = ConsoleColor.DarkCyan;
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Hallo wereld, wereld,\nDe wereld is van mij.\nEr is ruimte zat,\nDus kom er lekker bij.\nHallo wereld, wereld,\nDe wereld is voor jou\nEr zijn wel duizend kleuren\nVeel meer dan rood, wit, blauw\nEn wat ik doe, doe, doe\nDoe ik samen met jou.\nEn waar ik ga, ga, ga,\nGa ik samen met jou\nHallo wereld\nHééééy\nJa jij,\nJe bent van ons allemaal.\n");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
