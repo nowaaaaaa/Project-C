b22c36ef72290b9a2a34af99ec96edf81b005fe6
fabiothomas <95278302+fabiothomas@users.noreply.github.com>
2022-10-06 13:54:58 +0200

    .

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p  className='tekst'>
+        <p  className='Frontlayer'>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <h1>
