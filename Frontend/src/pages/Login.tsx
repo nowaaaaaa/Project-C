d51d38bb79384b264daa8f11d5c63d4cd590ec3f
fabiothomas <95278302+fabiothomas@users.noreply.github.com>
2022-10-06 15:12:18 +0200

    darkmode

--- Frontend/src/pages/Login.tsx
+++ Frontend/src/pages/Login.tsx
@@ -1,27 +1,38 @@
 import React from 'react';
-import logo from '../logo.svg';
 import '../App.css';
 
 export function Login() {
+  const root = document.getElementsByTagName("html")[0];
+  const toggleDarkMode = () => {
+    root.setAttribute("class", "dark");
+    localStorage.theme = "dark";
+  }
+  const toggleLightMode = () => {
+    root.removeAttribute("class");
+    localStorage.theme = "light";
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p  className='Frontlayer'>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <h1>
+        <h1 className='bg-white '>
           Login enzo
         </h1>
         <a
-          className="App-link"
+          className="text-pink-400 dark:text-orange-500 "
           href="https://minecraft.net"
           target="_blank"
           rel="noopener noreferrer"
         >
           Minecraft
         </a>
+      
+        <div className='bg-white dark:bg-green-800 py-9 px-16 dark:py-2 dark:px-36 '></div>
       </header>
+      <button onClick={toggleLightMode}>light</button>
+      <button onClick={toggleDarkMode}>dark</button>
     </div>
   );
 }
