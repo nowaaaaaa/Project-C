9031f20ddc117cf97979000bf28de8d5ad833aed
kieran20022 <kieranvdheijden@gmail.com>
2022-10-18 17:57:02 +0200

    kleine fix

--- Frontend/src/routes.js
+++ Frontend/src/routes.js
@@ -2,7 +2,6 @@ import { Homepage } from './Pages/Homepage/Homepage';
 import { Login } from './Pages/Login/Login';
 import {Machines} from './Pages/Machines/Machines';
 import {ProblemSolver} from './Pages/ProblemSolver/ProblemSolver';
-import { Machines } from './Pages/Machines/Machines';
 import { Userpage } from './Pages/Userpage/Userpage';
 
 export const routes = [
