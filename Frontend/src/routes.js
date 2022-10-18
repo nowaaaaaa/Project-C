5aa7b57a9dabb9ee571129c4200ba1eaa84afc2c
nowa <57222741+nowaaaaaa@users.noreply.github.com>
2022-10-18 17:25:34 +0200

    Login iets beter & userpage aangemaakt
    kleine beetje improvement en nieuwe pagiena

--- Frontend/src/routes.js
+++ Frontend/src/routes.js
@@ -1,9 +1,11 @@
 import { Homepage } from './Pages/Homepage/Homepage';
 import { Login } from './Pages/Login/Login';
-import {Machines} from './Pages/Machines/Machines';
+import { Machines } from './Pages/Machines/Machines';
+import { Userpage } from './Pages/Userpage/Userpage';
 
 export const routes = [
     { path: '/', name: 'homepage', component: <Homepage />},
     { path: '/login', name: 'login', component: <Login />},
     { path: '/machines', name:'machines', component: <Machines />},
+    { path: '/userpage', name:'userpage', component: <Userpage />}
 ]
