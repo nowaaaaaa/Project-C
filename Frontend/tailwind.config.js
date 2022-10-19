67991eb65ba76a20bd961686e61ce32544e131c6
kieran20022 <kieranvdheijden@gmail.com>
2022-10-19 22:12:17 +0200

    font

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode:'class',
   theme: {
     extend: {
       fontFamily: {
-        'roboto': ['Roboto Condensed']
+        lora: "'Lora', serif",
       },
       colors: {
         vBlue:'#009fe3',
