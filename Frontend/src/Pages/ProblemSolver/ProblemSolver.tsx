57be722b15f310f40fb88094f9fd4cd898257e8e
kieran20022 <kieranvdheijden@gmail.com>
2022-10-27 20:42:39 +0200

    problemsolver

--- Frontend/src/Pages/ProblemSolver/ProblemSolver.tsx
+++ Frontend/src/Pages/ProblemSolver/ProblemSolver.tsx
@@ -11,7 +11,13 @@ export function ProblemSolver() {
       <div className='filler-space bg-white dark:bg-slate-800'>
         <div className='centerbox bg-white dark:bg-slate-600'>
           <div>
-            <textarea className='input1 bg-gray-100 dark:bg-sky-900' placeholder='Your problem'/>
+            <textarea className='input1 bg-gray-100 dark:bg-sky-900' placeholder='Describe your problem'/>
+            <br />
+            <textarea className='input1 bg-gray-100 dark:bg-sky-900' placeholder='Describe what is supposed to happen'/>
+            <br />
+            <label htmlFor="machineOn dark:accent-slate-600">Is your machine on?</label>
+            <br />
+            <input type="checkbox" className='machineOn dark:accent-slate-600' />
           </div>
         </div>
       </div>
