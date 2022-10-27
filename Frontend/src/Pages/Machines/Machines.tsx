1650fe5db30498fab6f5922fd1ab9fd551274a22
kieran20022 <kieranvdheijden@gmail.com>
2022-10-27 20:30:14 +0200

    kleuir

--- Frontend/src/Pages/Machines/Machines.tsx
+++ Frontend/src/Pages/Machines/Machines.tsx
@@ -11,13 +11,13 @@ export function Machines() {
       <div className='filler-space bg-white dark:bg-slate-800' />
         <div className='centerBox bg-white dark:bg-slate-700'>
           <h1>Your Machines</h1>
-          <div className='machine bg-vBlue dark:bg-orange-700'>
+          <div className='machine bg-vBlue'>
             <input type="radio" name='machacc' id='mach1' className='checkbox'/>
             <label htmlFor="mach1" className='accordionLabel font-lora'>Machine 1</label>
-            <p className='accText bg-white dark:bg-orange-600'>Problem 1</p>
-            <p className='accText bg-white dark:bg-orange-600'>Problem 2</p>
-            <p className='accText bg-white dark:bg-orange-600'>Problem 3</p>
-            <a href="problemsolver" className='accResolve bg-red-800 hover:bg-rose-800'> Problem Not Resolved?</a>
+            <p className='accText bg-white dark:bg-sky-400'>Problem 1</p>
+            <p className='accText bg-white'>Problem 2</p>
+            <p className='accText bg-white'>Problem 3</p>
+            <a href="problemsolver" className='accResolve bg-red-700 hover:bg-rose-800'> Problem Not Resolved?</a>
             <br className='accBr' />
             <br className='accBr' />
           </div>
