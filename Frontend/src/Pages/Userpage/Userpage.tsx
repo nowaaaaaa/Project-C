fa4dc272cb02d8a50217b868ef2c0d39f87498fe
nowa <57222741+nowaaaaaa@users.noreply.github.com>
2022-11-15 18:33:08 +0100

    beter

--- Frontend/src/Pages/Userpage/Userpage.tsx
+++ Frontend/src/Pages/Userpage/Userpage.tsx
@@ -14,9 +14,9 @@ export function Userpage() {
   return (
     <body className="userpage-body">
       < Navbar />
-      <div className='user-container bg-white dark:bg-slate-800 h-[90.5vh]'> 
+      <div className='user-container bg-white dark:bg-slate-800 h-[90.5vh] grid place-items-center'> 
         <div className="text-6xl pb-10 text-cyan-800 dark:text-cyan-400 text-center pt-24 font-helvetica" >Welcome, Harm de Boer from Boer Harm BV!</div> 
-        <button className='problemButton bg-vBlue text-xl text-white hover:bg-vBlueHover dark:bg-slate-400 rounded-lg w-1/4 h-[5vh] border-2 border-vBlue mt-12 mb-12' onClick={problem}>Do you have a problem with a machine?</button>
+        <button className='hover:rounded-2xl transition-all ease-in-out duration-200 rounded-xl bg-vBlue dark:bg-slate-500 text-xl text-white dark:text-cyan-400 hover:bg-vBlueHover dark:hover:bg-slate-600 w-1/4 h-[5vh] mt-8 mb-12' onClick={problem}>Do you have a problem with a machine?</button>
         {UserpageButtons("Client")} 
       </div>    
       < Footer />
--- Frontend/src/Pages/Userpage/UserpageButtons.tsx
+++ Frontend/src/Pages/Userpage/UserpageButtons.tsx
@@ -14,14 +14,14 @@ export function UserpageButtons(user: string) {
 
   return (
     <>
-      <div className="userpageButtons bg-white dark:bg-slate-800 divide-y">
+      <div className="userpageButtons bg-white dark:bg-slate-800">
         <button className="flex w-full h-1/6" onClick={buttons[0][1]}>
-          <div className='w-1/3'>{buttons[0][0]}</div>
+          <div className='w-1/3 text-cyan-800 dark:text-cyan-400'>{buttons[0][0]}</div>
           <img className='w-1/3 object-scale-down'src='https://www.thebalancemoney.com/thmb/1eNZeoFI_ON4BpTQXdHMihOtMQ8=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Employees-3117af3d30fa438bb6dd3ad4141a8b4c.jpg'></img> 
           <div className='w-1/3'></div>
         </button>
         <button className="flex w-full h-1/6" onClick={buttons[1][1]}>
-          <div className='w-1/3'>{buttons[1][0]}</div>
+          <div className='w-1/3 text-cyan-800 dark:text-cyan-400'>{buttons[1][0]}</div>
           <img className='w-1/3 object-scale-down'src='https://ictzaak.nl/wp-content/uploads/2019/06/Helpdesk-ICTzaak-Blogpage.png'></img> 
           <div className='w-1/3'></div>
         </button>
