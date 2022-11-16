c154c1b31c8350daa73bea902982e5a8fc492d80
nowa <57222741+nowaaaaaa@users.noreply.github.com>
2022-11-16 22:03:43 +0100

    userpage mobile friendly

--- Frontend/src/Pages/Userpage/Userpage.tsx
+++ Frontend/src/Pages/Userpage/Userpage.tsx
@@ -15,8 +15,8 @@ export function Userpage() {
     <body className="userpage-body">
       < Navbar />
       <div className='user-container bg-white dark:bg-slate-800 h-[90.5vh] grid place-items-center'> 
-        <div className="md:text-6xl text-2xl pb-10 text-cyan-800 dark:text-cyan-400 text-center pt-16 font-helvetica" >Welcome, Harm de Boer from Boer Harm BV!</div> 
-        <button className='hover:rounded-2xl transition-all ease-in-out duration-200 rounded-xl bg-vBlue dark:bg-slate-500 md:text-xl text-sm text-white dark:text-cyan-400 hover:bg-vBlueHover dark:hover:bg-slate-600 md:w-1/4 w-[65vw] md:h-[5vh] h-[4vh] md:mt-8 mb-12' onClick={problem}>
+        <div className="md:text-6xl text-2xl pb-10 text-cyan-800 dark:text-cyan-400 text-center md:pt-16 font-helvetica" >Welcome, Harm de Boer from Boer Harm BV!</div> 
+        <button className='self-start hover:rounded-2xl transition-all ease-in-out duration-200 rounded-xl bg-vBlue dark:bg-slate-500 md:text-xl text-sm text-white dark:text-cyan-400 hover:bg-vBlueHover dark:hover:bg-slate-600 md:w-1/4 w-[65vw] md:h-[5vh] h-[4vh] md:mt-8 md:mb-12' onClick={problem}>
           Do you have a problem to report?
         </button>
         {UserpageButtons("Client")} 
--- Frontend/src/Pages/Userpage/UserpageButtons.tsx
+++ Frontend/src/Pages/Userpage/UserpageButtons.tsx
@@ -14,16 +14,19 @@ export function UserpageButtons(user: string) {
 
   return (
     <>
-      <div className="userpageButtons bg-white dark:bg-slate-800">
-        <button className="flex" onClick={buttons[0][1]}>
-          <div className='w-1/3 text-4xl py-40 text-cyan-800 dark:text-cyan-400 border-b-2'>{buttons[0][0]}</div>
-          <img className='w-1/3 h-1/6 object-cover'src='https://www.thebalancemoney.com/thmb/1eNZeoFI_ON4BpTQXdHMihOtMQ8=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Employees-3117af3d30fa438bb6dd3ad4141a8b4c.jpg'></img> 
-          <div className='w-1/3 text-2xl py-40 text-cyan-800 dark:text-cyan-400 border-b-2'>{buttons[0][2]}</div>
+      <div className="userpageButtons md:self-auto self-start bg-white dark:bg-slate-800">
+      {/*<img className='block md:hidden w-1/2 object-cover'src='https://www.thebalancemoney.com/thmb/1eNZeoFI_ON4BpTQXdHMihOtMQ8=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Employees-3117af3d30fa438bb6dd3ad4141a8b4c.jpg' onClick={buttons[0][1]}></img> 
+      */}
+        <button className="flex items-center h-1/4 md:flex-nowrap flex-wrap-reverse" onClick={buttons[0][1]}>
+          <div className='md:w-1/3 w-1/2 md:text-4xl text-xl text-cyan-800 dark:text-cyan-400'>{buttons[0][0]}</div>
+          <img className='md:w-1/3 w-1/2'src='https://www.thebalancemoney.com/thmb/1eNZeoFI_ON4BpTQXdHMihOtMQ8=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Employees-3117af3d30fa438bb6dd3ad4141a8b4c.jpg'></img> 
+          <div className='md:w-1/3 md:mb-0 mb-4 w-full md:text-2xl text-sm text-cyan-800 dark:text-cyan-400'>{buttons[0][2]}</div>
         </button>
-        <button className="flex" onClick={buttons[1][1]}>
-          <div className='w-1/3 text-4xl py-40 text-cyan-800 dark:text-cyan-400 border-t-2'>{buttons[1][0]}</div>
-          <img className='w-1/3 h-1/6 object-cover'src='https://ictzaak.nl/wp-content/uploads/2019/06/Helpdesk-ICTzaak-Blogpage.png'></img> 
-          <div className='w-1/3 text-2xl py-40 text-cyan-800 dark:text-cyan-400 border-t-2'>{buttons[1][2]}</div>
+        <button className="flex items-center h-1/4 md:flex-nowrap flex-wrap" onClick={buttons[1][1]}>
+          <img className='block mb-4 md:hidden w-1/2'src='https://ictzaak.nl/wp-content/uploads/2019/06/Helpdesk-ICTzaak-Blogpage.png'></img> 
+          <div className='md:w-1/3 w-1/2 md:text-4xl text-xl text-cyan-800 dark:text-cyan-400'>{buttons[1][0]}</div>
+          <img className='md:block hidden w-1/3'src='https://ictzaak.nl/wp-content/uploads/2019/06/Helpdesk-ICTzaak-Blogpage.png'></img> 
+          <div className='md:w-1/3 w-full md:text-2xl text-sm text-cyan-800 dark:text-cyan-400'>{buttons[1][2]}</div>
         </button>
       </div>
     </>
