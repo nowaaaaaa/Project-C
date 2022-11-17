74d76608693277ca7c0134dfe8a1064d2626000f
nowa <57222741+nowaaaaaa@users.noreply.github.com>
2022-11-17 10:19:59 +0100

    Update VisconPage.tsx

--- Frontend/src/Pages/VisconPage/VisconPage.tsx
+++ Frontend/src/Pages/VisconPage/VisconPage.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import './VisconPage.css';
-import {DragDropContext, Draggable, Droppable} from 'react-beautiful-dnd'
-import './VisconPage'
 
 import { Navbar } from '../../Components/Navbar/Navbar'
 
