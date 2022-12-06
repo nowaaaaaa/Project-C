6b6eaac963fe64c12816ca8958fed1bfe2839934
kieran20022 <kieranvdheijden@gmail.com>
2022-12-06 20:30:01 +0100

    beetje kloten met component/functie

--- Frontend/src/Pages/Machines/Machines.tsx
+++ Frontend/src/Pages/Machines/Machines.tsx
@@ -1,64 +1,57 @@
+// import './Machines.css';
 import React from 'react';
-import './Machines.css';
+
 
 import { Navbar } from '../../Components/Navbar/Navbar'
-import{ Footer } from '../../Components/Footer/Footer'
 
-function prob(problem:string, solution:string) {
+
+type problem = {
+  problem: string,
+  solution: string;
+}
+
+type Machine = {
+  type: string,
+  name: string,
+  problems: problem[];
+}
+
+function getMachine(mach: Machine) {
   return (
-    <li className=''>
-      <h2>{problem}</h2>
-      <p className='dark:text-cyan-500 ml-4'>{solution}</p>
-    </li>
+    <div className='bg-slate-300 dark:bg-slate-600 dark:text-cyan-400 mx-auto w-1/2 mb-4'>
+      <h1 className='text-xl bg-slate-400 dark:bg-slate-700'>{mach.name}</h1>
+      <>{mach.problems.map((problem) => 
+          <>
+          <div className='border-b border-slate-700 mb-2 pb-2'>
+            <h2 className='text-left ml-4'>{problem.problem}</h2>
+            <p className='ml-10 text-left'>{problem.solution}</p>
+          </div>
+          </>
+      )}
+      </>
+    </div>
   )
+}
+
+const problem1: problem = {
+  problem: "The lift is stuck",
+  solution: "Press the button"
+}
+
+const machinenumber1: Machine = {
+  type: "Lift",
+  name: "Lift5A",
+  problems: [problem1, problem1, problem1, problem1, problem1, problem1, problem1]
 }
 
 export function Machines() {
   return (
-    <body>
+    <>
       < Navbar />
-      <div className='filler-space bg-white dark:bg-slate-800 ' />
-        <div className='centerBox '>
-          <h1 className='text-slate-700 dark:text-cyan-400 text-2xl'>Your Machines</h1>
-          <div className='machine bg-slate-200 dark:bg-slate-600 transition-all ease-in-out duration-500 hover:bg-slate-300 dark:slate-500 text-slate-700 dark:text-cyan-400'>
-            <input type="checkbox" name='machacc' id='mach1' className='checkbox'/>
-            <label htmlFor="mach1" className='accordionLabel'>Machine 1</label>
-            <ul className='accText text-left flex-col gap-4'>
-              {prob("Problem 1", "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias quis soluta, exercitationem inventore architecto rerum suscipit aut, repellat dolores debitis repudiandae, esse sunt? Minus dolorem iure, quae fuga voluptas quibusdam?")}
-              {prob("Problem 2", "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias quis soluta, exercitationem inventore architecto rerum suscipit aut, repellat dolores debitis repudiandae, esse sunt? Minus dolorem iure, quae fuga voluptas quibusdam?")}
-              {prob("Problem 3", "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias quis soluta, exercitationem inventore architecto rerum suscipit aut, repellat dolores debitis repudiandae, esse sunt? Minus dolorem iure, quae fuga voluptas quibusdam?")}
-            </ul>
-            <a href="problemsolver" className='accResolve bg-red-600 hover:rounded-2xl transition-all ease-in-out duration-200 hover:bg-rose-800 text-gray-200 rounded-xl'> Problem still not resolved?</a>
-            <br className='accBr' />
-            <br className='accBr' />
-          </div>
-          <div className='machine bg-slate-200 dark:bg-slate-600 transition-all ease-in-out duration-500 hover:bg-slate-300 dark:slate-500 text-slate-700 dark:text-cyan-400'>
-            <input type="checkbox" name='machacc' id='mach2' className='checkbox'/>
-            <label htmlFor="mach2" className='accordionLabel'>Machine 2</label>
-            <ul className='accText text-left flex-col gap-4'>
-              {prob("Problem 1", "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias quis soluta, exercitationem inventore architecto rerum suscipit aut, repellat dolores debitis repudiandae, esse sunt? Minus dolorem iure, quae fuga voluptas quibusdam?")}
-              {prob("Problem 2", "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias quis soluta, exercitationem inventore architecto rerum suscipit aut, repellat dolores debitis repudiandae, esse sunt? Minus dolorem iure, quae fuga voluptas quibusdam?")}
-              {prob("Problem 3", "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias quis soluta, exercitationem inventore architecto rerum suscipit aut, repellat dolores debitis repudiandae, esse sunt? Minus dolorem iure, quae fuga voluptas quibusdam?")}
-            </ul>
-            <a href="problemsolver" className='accResolve bg-red-600 hover:rounded-2xl transition-all ease-in-out duration-200 hover:bg-rose-800 text-gray-200 rounded-xl'> Problem still not resolved?</a>
-            <br className='accBr' />
-            <br className='accBr' />
-          </div>
-          <div className='machine bg-slate-200 dark:bg-slate-600 transition-all ease-in-out duration-500 hover:bg-slate-300 dark:slate-500 text-slate-700 dark:text-cyan-400'>
-            <input type="checkbox" name='machacc' id='mach3' className='checkbox'/>
-            <label htmlFor="mach3" className='accordionLabel'>Machine 3</label>
-            <ul className='accText text-left flex-col gap-4'>
-              {prob("Problem 1", "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias quis soluta, exercitationem inventore architecto rerum suscipit aut, repellat dolores debitis repudiandae, esse sunt? Minus dolorem iure, quae fuga voluptas quibusdam?")}
-              {prob("Problem 2", "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias quis soluta, exercitationem inventore architecto rerum suscipit aut, repellat dolores debitis repudiandae, esse sunt? Minus dolorem iure, quae fuga voluptas quibusdam?")}
-              {prob("Problem 3", "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias quis soluta, exercitationem inventore architecto rerum suscipit aut, repellat dolores debitis repudiandae, esse sunt? Minus dolorem iure, quae fuga voluptas quibusdam?")}
-            </ul>
-            <a href="problemsolver" className='accResolve bg-red-600 hover:rounded-2xl transition-all ease-in-out duration-200 hover:bg-rose-800 text-gray-200 rounded-xl'> Problem still not resolved?</a>
-            <br className='accBr' />
-            <br className='accBr' />
-          </div>
-        </div>
-      < Footer />
-    </body>
+      <div className='filler-space bg-white dark:bg-slate-800'>
+      {getMachine(machinenumber1)}
+      </div>
+    </>
   );
 }
 
---
+++ Frontend/src/Pages/Machines/MakeMachine.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+
+export default function MakeMachine() {
+  return (
+    <div>
+
+    </div>
+  )
+}
