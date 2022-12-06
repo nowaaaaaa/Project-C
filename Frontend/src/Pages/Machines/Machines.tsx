5ef75351345e183eab09d2ae6370f6d3e504c75b
kieran20022 <kieranvdheijden@gmail.com>
2022-12-07 00:18:23 +0100

    Component voor machines en niet-css accordion yeahhhhh

--- Frontend/src/Pages/Machines/Machines.tsx
+++ Frontend/src/Pages/Machines/Machines.tsx
@@ -1,41 +1,11 @@
-// import './Machines.css';
+import './Machines.css';
 import React from 'react';
-
-
+import { MakeMachine, Machine, problem } from './MakeMachine'
 import { Navbar } from '../../Components/Navbar/Navbar'
 
-
-type problem = {
-  problem: string,
-  solution: string;
-}
-
-type Machine = {
-  type: string,
-  name: string,
-  problems: problem[];
-}
-
-function getMachine(mach: Machine) {
-  return (
-    <div className='bg-slate-300 dark:bg-slate-600 dark:text-cyan-400 mx-auto w-1/2 mb-4'>
-      <h1 className='text-xl bg-slate-400 dark:bg-slate-700'>{mach.name}</h1>
-      <>{mach.problems.map((problem) => 
-          <>
-          <div className='border-b border-slate-700 mb-2 pb-2'>
-            <h2 className='text-left ml-4'>{problem.problem}</h2>
-            <p className='ml-10 text-left'>{problem.solution}</p>
-          </div>
-          </>
-      )}
-      </>
-    </div>
-  )
-}
-
 const problem1: problem = {
-  problem: "The lift is stuck",
-  solution: "Press the button"
+  problem: "The shuttle is stuck",
+  solution: "Move items on the shuttle cable"
 }
 
 const machinenumber1: Machine = {
@@ -44,12 +14,19 @@ const machinenumber1: Machine = {
   problems: [problem1, problem1, problem1, problem1, problem1, problem1, problem1]
 }
 
+const pro = [problem1, problem1];
+
 export function Machines() {
   return (
     <>
       < Navbar />
-      <div className='filler-space bg-white dark:bg-slate-800'>
-      {getMachine(machinenumber1)}
+      <div className='a min-h-screen h-max bg-white dark:bg-slate-800'>
+        <MakeMachine type="Shuttle" name="Shuttle F6" problems={pro}/>
+        <MakeMachine type="Shuttle" name="Shuttle F6" problems={pro}/>
+        <MakeMachine type="Shuttle" name="Shuttle F6" problems={pro}/>
+        <MakeMachine type="Shuttle" name="Shuttle F6" problems={pro}/>
+        <MakeMachine type="Shuttle" name="Shuttle F6" problems={pro}/>
+        <MakeMachine type="Shuttle" name="Shuttle F6" problems={pro}/>
       </div>
     </>
   );
--- Frontend/src/Pages/Machines/MakeMachine.tsx
+++ Frontend/src/Pages/Machines/MakeMachine.tsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react';
 
+export type problem = {
+  problem: string,
+  solution: string;
+}
 
+export type Machine = {
+  type: string,
+  name: string,
+  problems: problem[];
+}
 
-export default function MakeMachine() {
+export function MakeMachine(mach: Machine) {
+  const [isActive, setIsActive] = useState(false);
   return (
-    <div>
-
+    <div className='pt-5'>
+      <div className='bg-slate-300 dark:bg-slate-600 dark:text-cyan-400 mx-auto w-1/2'>
+        <h1 className='header text-xl text-center bg-slate-400 dark:bg-slate-700' onClick={() => setIsActive(!isActive)}>{mach.name}</h1>
+        {isActive && <>{mach.problems.map((problem) => 
+            <>
+            <div className='border-b border-slate-700'>
+              <h2 className='text-left ml-4'>{problem.problem}</h2>
+              <p className='ml-10 text-left'>{problem.solution}</p>
+            </div>
+            </>
+        )}
+        </>}
+      </div>
     </div>
   )
 }
