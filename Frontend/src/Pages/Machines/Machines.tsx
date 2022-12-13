52532276d86e253035df7490ea377a1f1bc28ed7
nowa <57222741+nowaaaaaa@users.noreply.github.com>
2022-12-13 17:23:31 +0100

    Update Machines.tsx

--- Frontend/src/Pages/Machines/Machines.tsx
+++ Frontend/src/Pages/Machines/Machines.tsx
@@ -57,9 +57,6 @@ export function Machines() {
         <form className='w-[800px] justify-self-center'>   
           <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
           <div className="relative">
-            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-              <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
-            </div>
             <input type="search" id="default-search" className="outline-0 block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-vBlue  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required></input>
           <button type="submit" className="absolute right-2.5 bottom-2.5 focus:outline-none focus:ring-blue-300 font-medium rounded-lg px-4 py-2 hover:rounded-xl transition-all ease-in-out duration-200 bg-vBlue dark:bg-slate-500 text-lg text-white dark:text-cyan-400 hover:bg-vBlueHover dark:hover:bg-slate-600">Search</button>
         </div>
