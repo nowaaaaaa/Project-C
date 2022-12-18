07ff667e89ba21e4a0c74f5dd1f053de16f1bff9
nowa <57222741+nowaaaaaa@users.noreply.github.com>
2022-12-18 15:20:11 +0100

    snap er nog steeds niks van, help

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,55 +1,63 @@
 using Microsoft.OpenApi.Models;
-/*using Backend.EF;
-var db = new MyContext();
-if (db.companies.Count() == 0 && db.users.Count() == 0) {
-    db.companies.Add(new Company() { id = new Guid.NewGuid(), name = "Boer Harm BV", email = "admin", password = "admin", role = "customer"});
-    db.users.Add(new User() { id = new Guid.NewGuid(), name = "Harm de Boer", address = "Boerstaat 1", machines = new List<Machine>() });
-    saveChanges(db);
+using Microsoft.EntityFrameworkCore;
+using Backend.EF;
+namespace Backend {
+    class Program {
+        static void Main(string[] args) {
+            var db = new MyContext();
+            if (db.companies.Count() == 0 && db.users.Count() == 0) {
+                db.companies.Add(new Company() { id = new Guid.NewGuid(), name = "Boer Harm BV", email = "admin", password = "admin", role = "customer"});
+                db.users.Add(new User() { id = new Guid.NewGuid(), name = "Harm de Boer", address = "Boerstaat 1", machines = new List<Machine>() });
+                saveChanges(db);
+            }
+            /*
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var provider = builder.Services.BuildServiceProvider();
+            var configuration = provider.GetRequiredService<IConfiguration>();
+
+            var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+            builder.Services.AddCors(options => {
+                var frontendURL = configuration.GetValue<string>("frontend_url");
+
+                // options.AddDefaultPolicy(builder => {
+                //     builder.WithOrigins(frontendURL).AllowAnyMethod().AllowAnyHeader();
+                // });
+
+                options.AddPolicy(name: MyAllowSpecificOrigins,
+                                policy  =>
+                                {
+                                    policy.WithOrigins(frontendURL).AllowAnyHeader().AllowAnyMethod().AllowCredentials();
+                                });
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseCors(MyAllowSpecificOrigins);
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+            */
+        }
+    }
 }
-*/
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var provider = builder.Services.BuildServiceProvider();
-var configuration = provider.GetRequiredService<IConfiguration>();
-
-var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
-
-builder.Services.AddCors(options => {
-    var frontendURL = configuration.GetValue<string>("frontend_url");
-
-    // options.AddDefaultPolicy(builder => {
-    //     builder.WithOrigins(frontendURL).AllowAnyMethod().AllowAnyHeader();
-    // });
-
-    options.AddPolicy(name: MyAllowSpecificOrigins,
-                      policy  =>
-                      {
-                          policy.WithOrigins(frontendURL).AllowAnyHeader().AllowAnyMethod().AllowCredentials();
-                      });
-});
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseCors(MyAllowSpecificOrigins);
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
