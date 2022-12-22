a229aecbcafaf420d8e14a9db2be9cebee59f1e9
fabiothomas <95278302+fabiothomas@users.noreply.github.com>
2022-12-22 14:52:55 +0100

    wow

--- Backend/Controllers/AuthenticationController.cs
+++ Backend/Controllers/AuthenticationController.cs
@@ -10,6 +10,12 @@ namespace Backend.Controllers {
       public string password { get; set; } = null!;
     }
 
+    public class SignupInfo {
+      public string email { get; set; }
+      public string username { get; set; }
+      public string password { get; set; }
+    }
+
     private readonly ILogger<WeatherForecastController> _logger;
     public AuthenticationController(ILogger<WeatherForecastController> logger)
     {
@@ -18,16 +24,64 @@ namespace Backend.Controllers {
 
     [HttpPost]
     [Route("login")]
-    public IActionResult PostData(LoginInfo data) {
+    public IActionResult Login(LoginInfo data) {
       try {
         System.Console.WriteLine(data.email);
 
-        return Ok("Thank you for traveling with fiti, fabiothomas intercomputer travel incorporated");
+        return Ok("Thank you for traveling with fabiothomas intercomputer travel incorporated");
       }
       catch(Exception ex) {
         return BadRequest(ex.Message);
       }
-      return Ok("Not sure what happened");
+      return Ok("Not sure what happened...");
+    }
+
+    [HttpGet]
+    [Route("logout")]
+    public IActionResult Logout() {
+      return Ok("Successfully logged out.");
+    }
+
+    [HttpPost]
+    [Route("signup")]
+    public IActionResult Signup(SignupInfo data) {
+      try {
+        System.Console.WriteLine(data.email);
+
+        return Ok("Account now exists! wow!");
+      }
+      catch(Exception ex) {
+        return BadRequest(ex.Message);
+      }
+      return Ok("Not sure what happened...");
+    }
+
+    [HttpPut]
+    [Route("password")]
+    public IActionResult Password(string newpassword) {
+      try {
+        System.Console.WriteLine(newpassword);
+
+        return Ok("Password updated!");
+      }
+      catch(Exception ex) {
+        return BadRequest(ex.Message);
+      }
+      return Ok("Not sure what happened...");
+    }
+
+    [HttpPut]
+    [Route("username")]
+    public IActionResult Password(string newusername) {
+      try {
+        System.Console.WriteLine(newusername);
+
+        return Ok("Username updated!");
+      }
+      catch(Exception ex) {
+        return BadRequest(ex.Message);
+      }
+      return Ok("Not sure what happened...");
     }
   }
 }
