ff9608c2302b6c16b0a4402c9c55aae38dcd2b60
kieran20022 <kieranvdheijden@gmail.com>
2023-01-10 23:22:32 +0100

    a

--- Backend/Classes/EndpointModel.cs
+++ Backend/Classes/EndpointModel.cs
@@ -30,28 +30,28 @@ namespace Backend {
 //Weet niet zeker of ik dit moet doen maar ik kan verder op het moment niks...
 
   public class MachineType {
-    public Guid id { get; set; }
+    //public Guid id { get; set; }
     public string name { get; set; } = string.Empty;
     public string drawingNr { get; set; } = string.Empty;
   }
 
   public class Company {
-    public Guid id { get; set; }
+    //public Guid id { get; set; }
     public string name { get; set; } = string.Empty;
     public string address { get; set; } = string.Empty;
   }
 
   public class AckProblem {
-    public guid id { get; set; }
-    public MachineType type { get; set; } = string.Empty;
+    //public guid id { get; set; }
+    public MachineType type { get; set; }
     public string problem { get; set; } = string.Empty;
     public string solution { get; set; } = string.Empty;
   }
 
   public class Machine {
-    public Guid id { get; set; }
+    //public Guid id { get; set; }
     public string name { get; set; } = string.Empty;
-    public MachineType type { get; set; } = string.Empty;
+    public MachineType type { get; set; }
     public string companyId { get; set; } = string.Empty;
   }
 }
