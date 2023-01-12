19ab5404f428fb27ec40337a784c9e2574530c02
fabiothomas <95278302+fabiothomas@users.noreply.github.com>
2023-01-12 12:57:32 +0100

    quick fix

--- Frontend/src/Components/Userpage/UserpageButtons.tsx
+++ Frontend/src/Components/Userpage/UserpageButtons.tsx
@@ -4,16 +4,22 @@ import { Translate } from '../../Components/Languages/Translator';
 import { getRole } from '../../Pages/Login/AccountManager';
 
 export function UserpageButtons() {
+  var role = 0;
+  var token = localStorage.getItem("token")
+  if (token != null) {
+    role = getRole(token);
+  }
+
   let buttons: [string, () => void, string, string][] = [];
   const navigate = useNavigate();
   const employees = () => navigate('../userCreation');
   const tickets = () => navigate('../visconPage');
   const companies = () => navigate('../employees');
   const viscon = () => navigate('../visconPage');
-  if (getRole() === 2) {
+  if (role === 2) {
     buttons.push([`${Translate('Manage Employees')}`, employees, `${Translate("You have")} ${Translate("no")} ${Translate("new notifications")}.`, 'https://www.thebalancemoney.com/thmb/1eNZeoFI_ON4BpTQXdHMihOtMQ8=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Employees-3117af3d30fa438bb6dd3ad4141a8b4c.jpg']);
     buttons.push([`${Translate('View Tickets')}`, tickets, `${Translate('Your company has issued')} 3 ${Translate('new tickets')}.`, 'https://ictzaak.nl/wp-content/uploads/2019/06/Helpdesk-ICTzaak-Blogpage.png'])
-  } else if (getRole() === 1) {
+  } else if (role === 1) {
     buttons.push([`${Translate('Manage Companies')}`, companies, `${Translate("You have")} ${Translate("no")} ${Translate("new notifications")}.`, 'https://www.thebalancemoney.com/thmb/1eNZeoFI_ON4BpTQXdHMihOtMQ8=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Employees-3117af3d30fa438bb6dd3ad4141a8b4c.jpg']);
     buttons.push([`${Translate('View Tickets')}`, viscon, `${Translate('There are')} 3 ${Translate('unclaimed tickets')}.`, 'https://ictzaak.nl/wp-content/uploads/2019/06/Helpdesk-ICTzaak-Blogpage.png'])
   }
