9af08a6d07ac03e41660beb9d11ed8ff5e0c2425
nowa <57222741+nowaaaaaa@users.noreply.github.com>
2023-01-18 16:02:59 +0100

    verbeteringen uit testen

--- Frontend/src/Pages/Machines/Machines.tsx
+++ Frontend/src/Pages/Machines/Machines.tsx
@@ -4,16 +4,25 @@ import { Navbar } from '../../Components/Navbar/Navbar'
 import { Translate } from '../../Components/Languages/Translator';
 import { useState } from 'react';
 import { getCompanyId } from '../../Pages/Login/AccountManager';
-import { GetMachinesEP, GetAckProblemsEP } from '../../BackendManager/endpoints';
+import { GetMachinesEP, GetAckProblemsEP, GetCompanyIdEP } from '../../BackendManager/endpoints';
 import {Guid} from 'guid-typescript';
 
 var machinesList: Machine[] = [];
 var ackProblems: AckProblem[] = [];
 var companyId = "";
+var companyName = "";
 var token = localStorage.getItem("token")
 if (token != null) {
   companyId = getCompanyId(token);
 }
+GetCompanyIdEP({
+  companyId: companyId
+}).then(response => {
+  companyName = response.data;
+
+}).catch(error => {
+  console.error(error)
+})
 GetMachinesEP({
   companyId: companyId
 }).then(response => {
@@ -40,8 +49,8 @@ function Search (keyword: string) {
   if (keyword === "") return machinesList;
   let res: Machine[] = [];
   machinesList.forEach(m => {
-    if (m.name.toLowerCase().search(keyword) !== -1 || m.machineTypeName.toLowerCase().search(keyword) !== -1
-    ) res.push(m);
+    if (m.name.toLowerCase().search(keyword) !== -1 || m.machineTypeName.toLowerCase().search(keyword) !== -1 ||
+     Translate(m.machineTypeName).toLowerCase().search(keyword) !== -1) res.push(m);
   });
   return res;
 }
@@ -54,21 +63,20 @@ export function Machines() {
   const handleChange = (event: React.FormEvent<HTMLInputElement>) => {
     setKeyword(event.currentTarget.value.toLowerCase());
   };
-
   return (
     <>
       <div className='bg-white dark:bg-slate-800 min-h-screen'>
       < Navbar />
         <div className='grid grid-cols-1 place-content-start h-max'>
           {/* Search bar */}
-          <form className='flex flex-row w-[800px] mt-5 mb-2 justify-self-center gap-x-4'>   
+          <form className='flex flex-row max-w-full w-[800px] mt-5 mb-2 justify-self-center gap-x-4'>   
             <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
             <div className="relative grow">
               <input type="search" id="default-search" onChange={handleChange} className="outline-0 block w-full p-2 text-sm border text-cyan-800 dark:text-slate-300 border-gray-300 rounded-lg bg-gray-50 focus:border-vBlue dark:bg-slate-600 dark:border-slate-500 dark:placeholder-slate-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={Translate("Search for machines or machinetypes") + "..."} required></input>
             </div>
-            <button type="submit" className="focus:outline-none focus:ring-blue-300 font-medium rounded-lg px-3 py-1 hover:rounded-xl transition-all ease-in-out duration-200 bg-vBlue dark:bg-slate-500 text-lg text-white dark:text-cyan-400 hover:bg-vBlueHover dark:hover:bg-slate-600">{Translate("Search")}</button>
           </form>
           {/* Search bar */}
+          <h1 className='justify-self-center text-slate-800 dark:text-cyan-400 md:w-3/5 text-2xl text-start mb-2'>{`${Translate("Machines of")} ${companyName}:`}</h1>
             {listMachines(Search(keyword))}
         </div>
       </div>
