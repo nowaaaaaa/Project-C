f46821431761611ae2cec53d175af74166f8686f
nowa <57222741+nowaaaaaa@users.noreply.github.com>
2023-01-18 15:06:02 +0100

    testen voltooid

--- Backend/Controllers/MachinesController.cs
+++ Backend/Controllers/MachinesController.cs
@@ -11,6 +11,19 @@ namespace Backend.Controllers {
     [Route("[controller]")]
     public class MachinesController : ControllerBase {
         [HttpPost]
+        [Route("getAckProblems")]
+        public async Task<IActionResult> GetAckProblems(GetAckProblemsDto data) {
+            try {
+                using (var context = new MyContext()) {
+                    List<AckProblem> ackProblems = await context.ackProblems.Where(p => p.machineTypeId == Guid.Parse(data.machineTypeId)).ToListAsync();
+                    return Ok(ackProblems);
+                }
+            }
+            catch(Exception ex) {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpPost]
         [Route("getMachines")]
         public async Task<IActionResult> GetMachines(GetMachinesDto data) {
             try {
@@ -22,10 +35,6 @@ namespace Backend.Controllers {
                         temp =  await context.machineTypes.Where(p => p.id == m.typeId).Select(p => p.name).FirstOrDefaultAsync();
                         machinesTS.Add(new MachineToSend(m, temp!));
                     }
-                    /*if (company == null) {
-                        return BadRequest("Company not found");
-                    }*/
-                    
                     return Ok(machinesTS);
                 }
             }
@@ -33,33 +42,6 @@ namespace Backend.Controllers {
                 return BadRequest(ex.Message);
             }
         }
-        [HttpPost]
-        [Route("getAckProblems")]
-        public async Task<IActionResult> GetAckProblems(GetAckProblemsDto data) {
-            try {
-                using (var context = new MyContext()) {
-                    List<AckProblem> ackProblems = await context.ackProblems.Where(p => p.machineTypeId == Guid.Parse(data.machineTypeId)).ToListAsync();
-                    /*if (company == null) {
-                        return BadRequest("Company not found");
-                    }*/
-                    
-                    return Ok(ackProblems);
-                }
-            }
-            catch(Exception ex) {
-                return BadRequest(ex.Message);
-            }
-        }
-
-        // static async Task<IEnumerable<T>> WhereAsync<T>(this IEnumerable<T> source, Func<T, Task<bool>> predicate) {
-        //     var results = new Queue<T>();
-        //     var tasks = source.Select(async x => {
-        //         if (await predicate(x)) results.Enqueue(x);
-        //     });
-        // await Task.WhenAll(tasks);
-        // return results;
-//          }
-
     }
 
     class MachineToSend : Machine {
--- Frontend/src/Pages/Machines/Machines.tsx
+++ Frontend/src/Pages/Machines/Machines.tsx
@@ -7,88 +7,12 @@ import { getCompanyId } from '../../Pages/Login/AccountManager';
 import { GetMachinesEP, GetAckProblemsEP } from '../../BackendManager/endpoints';
 import {Guid} from 'guid-typescript';
 
-//vvvvvvvvv==] Dummy Data [==vvvvvvvvv//
-
-// const guid1: Guid = Guid.create();
-// const guid2: Guid = Guid.create();
-
-// const machineType1 : MachineType = {
-//   name: "Lift",
-//   id: guid1
-// }
-
-// const machineType2 : MachineType = {
-//   name: "Shuttle",
-//   id: guid2
-// }
-
-// const problem1: AckProblem = {
-//   problem: Translate("The shuttle is stuck."),
-//   solution: Translate("Move items on the shuttle cable."),
-//   machineTypeId: machineType1.id
-// }
-
-// const problem2: AckProblem = {
-//   problem: Translate("The lift is stuck."),
-//   solution: Translate("Move items on the lift cable."),
-//   machineTypeId: machineType2.id
-// }
-
-// const problem3: AckProblem = {
-//   problem: Translate("Lift cable is broken"),
-//   solution: Translate("Install new cable"),
-//   machineTypeId: machineType2.id
-// }
-
-// const machinenumber1: Machine = {
-//   typeId: machineType1.id,
-//   name: "Lift5A",
-//   problems: [],
-//   type: machineType1
-// }
-// const machinenumber2: Machine = {
-//   typeId: machineType1.id,
-//   name: "Lift1",
-//   problems: [],
-//   type: machineType1
-// }
-// const machinenumber3: Machine = {
-//   typeId: machineType1.id,
-//   name: "Shuttle1",
-//   problems: [],
-//   type: machineType1
-// }
-// const machinenumber4: Machine = {
-//   typeId: machineType1.id,
-//   name: "Shuttle2",
-//   problems: [],
-//   type: machineType1
-// }
-// const machinenumber5: Machine = {
-//   typeId: machineType2.id,
-//   name: "Band1A",
-//   problems: [],
-//   type: machineType2
-// }
-// const machinenumber6: Machine = {
-//   typeId: machineType2.id,
-//   name: "Band1B",
-//   problems: [],
-//   type: machineType2
-// }
-//const probs : AckProblem[] = [problem1, problem2, problem3];
-
-// const machTestList: Machine[] = [machinenumber1, machinenumber2, machinenumber3, machinenumber4, machinenumber5, machinenumber6];
-
-// //^^^^^^^^^^==] Dummy Data [==^^^^^^^^^^^//
-
 var machinesList: Machine[] = [];
 var ackProblems: AckProblem[] = [];
 var companyId = "";
 var token = localStorage.getItem("token")
 if (token != null) {
   companyId = getCompanyId(token);
-  
 }
 GetMachinesEP({
   companyId: companyId
@@ -140,7 +64,7 @@ export function Machines() {
           <form className='flex flex-row w-[800px] mt-5 mb-2 justify-self-center gap-x-4'>   
             <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
             <div className="relative grow">
-              <input type="search" id="default-search" onChange={handleChange} className="outline-0 block w-full p-2 text-sm border text-cyan-800 dark:text-slate-300 border-gray-300 rounded-lg bg-gray-50 focus:border-vBlue dark:bg-slate-600 dark:border-slate-500 dark:placeholder-slate-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={Translate("Search for machines") + "..."} required></input>
+              <input type="search" id="default-search" onChange={handleChange} className="outline-0 block w-full p-2 text-sm border text-cyan-800 dark:text-slate-300 border-gray-300 rounded-lg bg-gray-50 focus:border-vBlue dark:bg-slate-600 dark:border-slate-500 dark:placeholder-slate-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={Translate("Search for machines or machinetypes") + "..."} required></input>
             </div>
             <button type="submit" className="focus:outline-none focus:ring-blue-300 font-medium rounded-lg px-3 py-1 hover:rounded-xl transition-all ease-in-out duration-200 bg-vBlue dark:bg-slate-500 text-lg text-white dark:text-cyan-400 hover:bg-vBlueHover dark:hover:bg-slate-600">{Translate("Search")}</button>
           </form>
