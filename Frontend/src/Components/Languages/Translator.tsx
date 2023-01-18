2b9ff939bace1cb465909fef1410f79e0b1aef9e
nowa <57222741+nowaaaaaa@users.noreply.github.com>
2023-01-18 22:19:16 +0100

    Update Translator.tsx

--- Frontend/src/Components/Languages/Translator.tsx
+++ Frontend/src/Components/Languages/Translator.tsx
@@ -13,7 +13,6 @@ var values = Object.values(langPack);
 let langMap:LanguageMap = {};
 for (let i = 0; i < names.length; i++) {    
     langMap[names[i]] = values[i];
-    console.log('hello');
 }
 export function Translate(str: string) {
     let res: string = str;
