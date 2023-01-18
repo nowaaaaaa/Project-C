8b12540dbe7b194114784dfc16ecd2c50dfeccaa
nowa <57222741+nowaaaaaa@users.noreply.github.com>
2023-01-19 00:16:46 +0100

    verbeteringen uit usability test

--- Frontend/src/Pages/Userpage/Userpage.tsx
+++ Frontend/src/Pages/Userpage/Userpage.tsx
@@ -44,8 +44,8 @@ export function Userpage() {
       < Navbar />
       <div className='user-container bg-white dark:bg-slate-800 grid place-items-center'> 
         <div className="md:text-6xl text-2xl pb-10 text-cyan-800 dark:text-cyan-400 text-center md:pt-16 font-helvetica" >{`${Translate("Welcome")}, ${name} ${Translate("from")} ${companyName}!`}</div> 
-        <button className='self-start hover:rounded-2xl transition-all ease-in-out duration-200 rounded-xl bg-vBlue dark:bg-slate-500 text-white dark:text-cyan-400 hover:bg-vBlueHover dark:hover:bg-slate-600 md:text-xl text-sm md:w-1/4 w-[65vw] md:h-[5vh] h-[4vh] md:mt-8 md:mb-12' onClick={problem}>
-          {Translate("Do you have a problem to report?")}
+        <button className='self-start hover:rounded-2xl transition-all ease-in-out duration-200 rounded-xl bg-vBlue dark:bg-slate-500 text-white dark:text-cyan-400 hover:bg-vBlueHover dark:hover:bg-slate-600 md:text-xl text-sm md:w-1/4 w-[80vw] md:h-[5vh] h-[4vh] md:mt-8 md:mb-12' onClick={problem}>
+          {Translate("Click here to report a problem.")}
         </button>
         {UserpageButtons()} 
       </div>    
