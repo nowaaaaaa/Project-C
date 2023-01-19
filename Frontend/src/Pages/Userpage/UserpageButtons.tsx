64eb1f737e4c4c5d0bfc6c344b2403ea2936e8b0
nowa <57222741+nowaaaaaa@users.noreply.github.com>
2023-01-19 20:11:22 +0100

    Update UserpageButtons.tsx

--- Frontend/src/Pages/Userpage/UserpageButtons.tsx
+++ Frontend/src/Pages/Userpage/UserpageButtons.tsx
@@ -40,14 +40,14 @@ export function UserpageButtons() {
       <div className="userpageButtons md:self-auto self-start bg-white dark:bg-slate-800">
         <button className="flex items-center h-1/4 md:flex-nowrap flex-wrap-reverse" onClick={buttons[0][1]}>
           <div className='md:w-1/3 w-1/2 md:text-4xl text-xl text-cyan-800 dark:text-cyan-400'>{buttons[0][0]}</div>
-          <img className='md:w-1/3 w-1/2'src={buttons[0][3]}></img> 
+          <img className='md:w-1/3 w-1/2'src={buttons[0][3]} alt="Happy colleagues"></img> 
           <div className='md:w-1/3 md:mb-0 mb-4 w-full md:text-2xl text-sm text-cyan-800 dark:text-cyan-400'>{buttons[0][2]}</div>
         </button>
         {buttons.length === 2 && (
         <button className="flex items-center h-1/4 md:flex-nowrap flex-wrap" onClick={buttons[1][1]}>
-          <img className='block mb-4 md:hidden w-1/2'src={buttons[1][3]}></img> 
+          <img className='block mb-4 md:hidden w-1/2'src={buttons[1][3]} alt="Tech support"></img> 
           <div className='md:w-1/3 w-1/2 md:text-4xl text-xl text-cyan-800 dark:text-cyan-400'>{buttons[1][0]}</div>
-          <img className='md:block hidden w-1/3'src={buttons[1][3]}></img> 
+          <img className='md:block hidden w-1/3'src={buttons[1][3]} alt="Tech support"></img> 
           <div className='md:w-1/3 w-full md:text-2xl text-sm text-cyan-800 dark:text-cyan-400'>{buttons[1][2]}</div>
         </button>
         )}
